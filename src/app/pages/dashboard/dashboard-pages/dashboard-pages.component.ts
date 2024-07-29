24103779b7c78a1b2ccb7fd05051c1fa129d5666
jmike-dev <gmikekamau@gmail.com>
2024-07-29 15:08:05 +0300

    ✨ created about, login, signup and  dashboard pages

---
+++ src/app/pages/dashboard/dashboard-pages/dashboard-pages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-pages',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-pages.component.html'
+})
+export class DashboardPagesComponent {
+
+}
