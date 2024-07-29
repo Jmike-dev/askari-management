c76593e6e53e0de38a0960db2073aef358e1c7d2
jmike-dev <gmikekamau@gmail.com>
2024-07-29 15:24:38 +0300

    🚚 defined my routes accordingly

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { LoginPageComponent } from './pages/auth/login-page/login-page.component';
+import { SignUpPageComponent } from './pages/auth/sign-up-page/sign-up-page.component';
+import { DashboardPagesComponent } from './pages/dashboard/dashboard-pages/dashboard-pages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'homePage', component: HomePageComponent },
+    { path: 'aboutPage', component: AboutPageComponent },
+    { path: 'loginPage', component: LoginPageComponent },
+    { path: 'SignPage', component: SignUpPageComponent },
+    { path: 'DashboardPage', component: DashboardPagesComponent },
+];
